8e65a75e33840cc36c196621a75ba15a4bc3c2f4
hanollo <hanollo@gmail.com>
2023-05-25 12:09:22 +0900

    에러 수정(줄바꿈 이슈)

--- src/main/index.js
+++ src/main/index.js
@@ -1,24 +1,22 @@
 function MainPage()
 {
-    return
-    (
+    return (
         <div>
-        <div id="header">
-            <div id="header-area">
-                <img src="images/icons/bside_logo.png"/>
+            <div id="header">
+                <div id="header-area">
+                    <img src="images/icons/bside_logo.png"/>
+                </div>
             </div>
+            <div id="body">
+                <div id="banner">
+                    <img src="images/banners/bn1.png"/>
+                </div>
+                <h1>진행중 캠페인</h1>
+                <div id="campaign-list">
+                </div>
+            </div>
+            <div id="footer"></div>
         </div>
-    <div id="body">
-        <div id="banner">
-            <img src="images/banners/bn1.png"/>
-        </div>
-        <h1>진행중 캠페인</h1>
-        <div id="campaign-list">
-        </div>
-    </div>
-
-    <div id="footer"></div>
-    </div>
     );
 }
 export default MainPage;
